8484b7655c7f9cff3d241aa8126fbb0138073c20
Nadia Aamand <nadiaaamand@gmail.com>
2016-10-04 13:54:59 +0200

    new page
    created a new page for specific project details - created connection
    and added project information

--- projectdetails.php
+++ projectdetails.php
@@ -24,32 +24,34 @@ $stmt->bind_result($cnam, $cadr, $ccnam, $ccphone, $czip);
 while($stmt->fetch()) { }
 
 echo '<h2>'.$cnam.'</h1>';
-	//combine to strings and make between them
-	echo '<h5>'.$cadr. ' ' .$czip.'</h5>';
-	echo '<h5>'.$ccnam.'</h5>';
-	echo '<h5>'.$ccphone.'</h5>';
 ?>
 </ul>
 
-<h2>Projects</h2>
+<h2>Project Details</h2>
 <ul>
 <?php 
 
-$sql = 'select `project-name`
+$sql = 'select `project-name`, `project-description`, `project-start-date`, `project-end-date`, `other-project-details`
 from `project`
 where `project-id` = ?
-and `client-id` = `client-id`';
+and `client-id` = `client-id`;';
 
 $stmt = $link->prepare($sql);
 $stmt->bind_param('i', $cid);
 $stmt->execute();
-$stmt->bind_result($pnam);
+$stmt->bind_result($pnam, $pdesc, $psd, $ped, $popid);
 
 while($stmt->fetch()) { 
-	echo '<li><a href="projectdetails.php?cid='.$cid.'">'.$pnam.'</a></li>';
+	echo '<h3>'.'Project name'.'</h3>';
+	echo '<p>'.$pnam.'</p>';
+	echo '<h3>'.'Project description'.'</h3>';
+	echo '<p>'.$pdesc.'</p>';
+	echo '<h3>'.'Project start date'.'</h3>';
+	echo '<p>'.$psd.'</p>';
+	echo '<h3>'.'Project end date'.'</h3>';
+	echo '<p>'.$ped.'</p>';
+	
 }
 ?>
-</ul>
-
 </body>
 </html>
